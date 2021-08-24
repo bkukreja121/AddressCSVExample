d93e7e77331317e12fe08e3f1d55055a64a9c0ea
Bhaveshk <Bhavishkukreja16@gmail.com>
2021-08-24 21:12:39 +0530

    AddressCSV

---
+++ AddressCSV/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CsvHelper;
+using System.IO;
+using System.Globalization;
+
+namespace CsvWriterDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var csvPath = Path.Combine(Environment.CurrentDirectory, @"C:\Users\bkukrej\source\repos\AddressCSV\AddressCSV\Uitility\Address.csv");
+            using (var streamWriter = new StreamWriter(csvPath))
+            {
+                using (var csvWriter = new CsvWriter(streamWriter, CultureInfo.InvariantCulture))
+                {
+                    var rockets = AddressInfo.GetAddress();
+                    csvWriter.Context.RegisterClassMap<AddressInfoClassMap>();
+                    csvWriter.WriteRecords(rockets);
+                }
+            }
+        }
+    }
+}
